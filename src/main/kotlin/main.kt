c095d33e2225480f1a14e3ac260a0a9a41a0f948
AlexZed777 <100261573+AlexZed777@users.noreply.github.com>
2022-03-19 13:11:02 +0400

    Исправление констант и счёта поумолчанию

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,20 @@
 
 fun main() {
-    println(comission(7000.00, 76000.00, vkPay))
+    println(comission(7000.00, 76000.00, MASTERCARD))
 }
 
-const val mastercard = "Mastercard"
-const val maestro = "Maestro"
-const val visa = "Visa"
-const val mir = "Мир"
-const val vkPay = "VK Pay"
+const val MASTERCARD = "Mastercard"
+const val MAESTRO = "Maestro"
+const val VISA = "Visa"
+const val MIR = "Мир"
+const val VKPAY = "VK Pay"
 
 
-fun comission(amountMoneysTransferInMonth: Double, moneysTransfer: Double, type: String): Double {
+fun comission(amountMoneysTransferInMonth: Double, moneysTransfer: Double, type: String = VKPAY): Double {
     return when (type) {
-        mastercard, maestro -> if (amountMoneysTransferInMonth < 75000.00 && moneysTransfer < 75000.00) 0.00
+        MASTERCARD, MAESTRO -> if (amountMoneysTransferInMonth < 75000.00 && moneysTransfer < 75000.00) 0.00
         else (moneysTransfer * 0.006) + 25.00
-        visa, mir -> if (moneysTransfer * 0.0075 < 35.00) 35.00 else moneysTransfer * 0.0075
-        vkPay -> 0.00
+        VISA, MIR -> if (moneysTransfer * 0.0075 < 35.00) 35.00 else moneysTransfer * 0.0075
         else -> 0.00
     }
 //    amountMoneysTransferInMonth + moneysTransfer ....тут мы потом будем прибавлять разовые переводы к сумме переводов за месяц
